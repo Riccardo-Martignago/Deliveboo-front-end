6b29c62ff27ccc57a2f4d1422a1b880db07f8dd6
afablelorenz <afablelorenz@gmail.com>
2024-09-12 15:46:22 +0200

    created navbar with restaurant route

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,17 @@
 <script>
 
     export default{
+            name: 'Header',
         data(){
             return{
                 linksName : [
                     {
                         label: "Home",
                         name: "home"
+                    },
+                    {
+                        label: "Restaurants",
+                        name: "restaurants"
                     }
                 ]
             }
@@ -16,10 +21,41 @@
 
 <template>
     <header>
-        <h1>HEADER</h1>
+        <nav>
+            <ul>
+                <li v-for="link in linksName" :key="link.name">
+                    <router-link :to="{ name: link.name }">
+                        {{ link.label }}
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
 <style scoped>
+header {
+    background-color: #333;
+    padding: 10px;
+}
 
+nav ul {
+    list-style: none;
+    display: flex;
+    justify-content: space-around;
+}
+
+nav ul li {
+    margin: 0 10px;
+}
+
+nav ul li a {
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+}
+
+nav ul li a:hover {
+    text-decoration: underline;
+}
 </style>
--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -3,6 +3,6 @@
 
 <template>
   <main>
-    <h1>home</h1>
+    <h1>Homepage</h1>
   </main>
 </template>
