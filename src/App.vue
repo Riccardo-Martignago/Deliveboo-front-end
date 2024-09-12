0ae9a2a05c37169b63d8735e5e986262675f1446
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-09-12 12:37:07 +0200

    Create setup vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+  import AppHeader from './components/AppHeader.vue';
+  import AppFooter from './components/AppFooter.vue';
+  export default{
+      components:{
+        AppHeader,
+        AppFooter,
+      },
+    }
+</script>
+
+<template>
+  <AppHeader/>
+  <RouterView/>
+  <AppFooter/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+
+    export default{
+        data(){
+            return{
+                linksName : [
+                    {
+                        label: "Home",
+                        name: "home"
+                    }
+                ]
+            }
+        },
+    }
+</script>
+
+<template>
+    <header>
+        <h1>HEADER</h1>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
