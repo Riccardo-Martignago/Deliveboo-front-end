6332903064ffb165b760468a98acf5b24c3b7f1d
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-09-17 17:05:37 +0200

    Removed comment

--- src/views/AppShop.vue
+++ src/views/AppShop.vue
@@ -2,12 +2,11 @@
 export default {
   data() {
     return {
-      cart: [],      // Array per i dati del carrello
-      cartTotal: 0,  // Totale del carrello
+      cart: [],
+      cartTotal: 0,
     };
   },
   methods: {
-    // Caricare il carrello dal localStorage
     loadCartFromLocalStorage() {
       const savedCart = localStorage.getItem('cart');
       if (savedCart) {
@@ -15,21 +14,17 @@ export default {
       }
     },
 
-    // Calcolare il totale del carrello
     calculateCartTotal() {
       this.cartTotal = this.cart.reduce((total, item) => {
         return total + item.price * item.quantity;
       }, 0);
     },
 
-    // Simulazione del pagamento
     simulatePayment() {
       alert(`Pagamento completato per un totale di € ${this.cartTotal}`);
-      // Puoi aggiungere qui logiche aggiuntive per resettare il carrello
       this.clearCart();
     },
 
-    // Svuotare il carrello (per esempio dopo il pagamento)
     clearCart() {
       this.cart = [];
       localStorage.removeItem('cart');
@@ -37,7 +32,6 @@ export default {
     }
   },
   created() {
-    // Caricare il carrello dal localStorage e calcolare il totale quando il componente viene creato
     this.loadCartFromLocalStorage();
     this.calculateCartTotal();
   },
@@ -47,7 +41,6 @@ export default {
 <template>
   <div>
     <h1>Riepilogo Acquisti</h1>
-    <!-- Lista dei prodotti nel carrello -->
     <ul>
       <li v-for="item in cart" :key="item.id">
         {{ item.name }} - Quantità: {{ item.quantity }} - Prezzo: €
@@ -55,10 +48,8 @@ export default {
       </li>
     </ul>
 
-    <!-- Totale del carrello -->
     <h3>Totale: € {{ cartTotal }}</h3>
 
-    <!-- Bottone per simulare il pagamento -->
     <button class="btn btn-primary" @click="simulatePayment">
       Procedi al pagamento
     </button>
@@ -66,6 +57,6 @@ export default {
 </template>
 
 <style scoped>
-/* Aggiungi qui il tuo CSS */
+
 </style>
 
